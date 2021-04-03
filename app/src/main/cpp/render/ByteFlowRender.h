6ee6165423b7c1e8294992f6084ce2e1ce85bd36
字节流动 <haohaochang86@gmail.com>
2021-04-03 14:46:22 +0800

    字节流动

--- app/src/main/cpp/render/ByteFlowRender.h
+++ app/src/main/cpp/render/ByteFlowRender.h
@@ -1,6 +1,10 @@
-//
-// Created by ByteFlow on 2019/4/15.
-//
+/**
+ *
+ * Created by 公众号：字节流动 on 2021/3/16.
+ * https://github.com/githubhaohao/OpenGLCamera2
+ * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
+ *
+ * */
 
 #ifndef OPENGLCAMERA2_BYTEFLOWRENDER_H
 #define OPENGLCAMERA2_BYTEFLOWRENDER_H
