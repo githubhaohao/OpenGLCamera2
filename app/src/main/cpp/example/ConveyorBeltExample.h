194b4ce81453e2ba8a524aef264ba4049cf6a726
字节流动 <haohaochang86@gmail.com>
2021-10-11 09:32:36 +0800

    update

--- app/src/main/cpp/example/ConveyorBeltExample.h
+++ app/src/main/cpp/example/ConveyorBeltExample.h
@@ -1,7 +1,7 @@
 /**
  *
  * Created by 公众号：字节流动 on 2022/8/18.
- * https://github.com/githubhaohao/NDK_OpenGLES_3_0
+ * https://github.com/githubhaohao/OpenGLCamera2
  * 最新文章首发于公众号：字节流动，有疑问或者技术交流可以添加微信 Byte-Flow ,领取视频教程, 拉你进技术交流群
  *
  * */
